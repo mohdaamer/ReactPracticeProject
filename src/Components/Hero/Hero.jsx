52c554f1f10fbf9209f59b6f5cdf121564ff1dc5
Aamer <mdaamer456@gmail.com>
2023-09-21 15:42:17 +0530

    fix: CountUp

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             {/* Left  */}
             <div>
                 <p className='text-[#20B486] font-medium text-2xl'>START TO SUCCESS</p>
-                <h1 className='py-4 text-4xl font-semibold leading-[50px]'>Access To <CountUp start={3000} end={5000} duration={4} className='text-[#20B486]'><span>5000</span></CountUp><span className='text-[#20B486]'>+</span> Courses <br/> from <CountUp start={0} end={300} duration={4} className='text-[#20B486]'><span>300</span></CountUp><span className='text-[#20B486]'>+</span> Instructors & Institutions</h1>
+                <h1 className='py-4 text-4xl font-semibold leading-[50px]'>Access To <CountUp start={3000} end={5000} duration={4} className='text-[#20B486]'><span>5000</span></CountUp><span className='text-[#20B486]'>+</span> Courses <br/> from <CountUp start={100} end={300} duration={4} className='text-[#20B486]'><span>300</span></CountUp><span className='text-[#20B486]'>+</span> Instructors & Institutions</h1>
                 <p className='py-4 text-gray-500 text-sm'>Various versions have evolved over the years, sometimes by accident,</p>
 
                 <form className='flex justify-between items-center shadow-md rounded-md max-w-[500px] p-2 my-6'>
