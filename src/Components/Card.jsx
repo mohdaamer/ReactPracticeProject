e1256f4a904321c359b1296af0cebbfc4ecad2b7
Aamer <mdaamer456@gmail.com>
2023-09-22 23:03:59 +0530

    Added: Slider

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,7 @@ import StarRating from './StarRating'
 
 const Card = () => {
   return (
-    <div className='bg-white drop-shadow-md overflow-hidden rounded-lg'>
+    <div className='bg-white drop-shadow-md overflow-hidden rounded-lg mr-2'>
       <img src="./images/blog-1.png" alt="" className='h-40 w-full object-cover' />
 
       <div className='p-5 border border-b'>
@@ -16,7 +16,11 @@ const Card = () => {
       </div>
 
       {/* Price  */}
-      <h3 className='p-5'>$500</h3>
+      <h3 className='p-5 text-xl'>$500</h3>
+
+      <div className='absolute top-0 m-3 bg-white p-2 rounded'>
+        <p className='font-semibold'>HTML</p>
+      </div>
     </div>
   )
 }
--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -1,11 +1,56 @@
 import React from "react";
 import Card from './Card';
+import Slider from "react-slick";
 
 function Courses() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
   return (
     <div className="w-full bg-[#E9F8F3B2] py-9">
-      <div className="container md:max-w-[1200px] m-auto max-w-[400px] px-6">
-        <Card />
+      <div className="container md:max-w-[1200px] m-auto max-w-[400px] px-6 overflow-hidden">
+        <div className="max-w-[200px] md:max-w-[1200px]">
+        <h1 className="text-4xl font-semibold py-3">Most Popular <span className="primaryText">Courses</span></h1>
+        <p className="text-[#6D737A] pb-12">Various versions have evolved over the years, sometimes by accident,</p>
+        </div>
+        <Slider {...settings}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </Slider>
       </div>
     </div>
   );
